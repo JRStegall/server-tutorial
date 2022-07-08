3153d9dc7fa1bf802aef3556b07af5b320dbeb7a
Jeremy Stegall <jeremystegall@Jeremys-MacBook-Pro.local>
2022-07-08 11:31:57 -0400

    Install nodemon, update app.js

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
-app.use((request, response) => {
+app.get(("/", request, response) => {
     response.json({ message: 'Hey ,dude!  This is your server response!  Good on you!' })
 });
+const cloudinary = require('cloudinary').v2
+cloudinary.config({
+    cloud_name: 'dlrpyxua6',
+    api_key: '445588491557849',
+    api_secret: 'ckuXwEbEDaVBffGEgkAoIErde4c',
+});
+
+api.post("/upload-image", (request, response) => {
+
+});
+
 module.exports = app;
