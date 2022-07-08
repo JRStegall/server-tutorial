6bad572f16039076bdbc3edfd8b988bc7bbd0289
Jeremy Stegall <jeremystegall@Jeremys-MacBook-Pro.local>
2022-07-08 09:45:36 -0400

    Activate server, add server response

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const http = require('http');
-const server = http.createServer();
+const server = http.createServer((Request, Response) => {
+    Response.end('Hey! This is your server response!');
+});
 server.listen(3000);
