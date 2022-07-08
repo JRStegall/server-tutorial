1738ae28c771edbff0a834cce0b6f7ceec535d01
Jeremy Stegall <jeremystegall@Jeremys-MacBook-Pro.local>
2022-07-08 11:39:06 -0400

    Modified app.js, package-lock.json, package.json

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const app = express();
-app.get(("/", request, response) => {
-    response.json({ message: 'Hey ,dude!  This is your server response!  Good on you!' })
-});
 const cloudinary = require('cloudinary').v2
+const bodyParser = require('body-parser');
+
+//BODY PARSER CONFIG
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//CLOUDINARY CONFIG
 cloudinary.config({
     cloud_name: 'dlrpyxua6',
     api_key: '445588491557849',
     api_secret: 'ckuXwEbEDaVBffGEgkAoIErde4c',
 });
 
-api.post("/upload-image", (request, response) => {
-
+app.get(("/", request, response) => {
+    response.json({ message: 'Hey ,dude!  This is your server response!  Good on you!' })
 });
+
+api.post("/upload-image", (request, response) => {
+    const data = {
+     image: request.body.image,
+    }
+    cloudinary.uploader.upload(data.image);
+});
+
+
+
+
 
 module.exports = app;
